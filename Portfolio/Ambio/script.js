2cef9bd7118c453b1f0c633121bd6b7b2123a925
Vladislava Suprotkina <vladislava.stk@gmail.com>
2019-07-29 18:45:57 +0300

    responsive

--- Portfolio/Ambio/script.js
+++ Portfolio/Ambio/script.js
@@ -5,3 +5,18 @@ $(document).ready(function(){
         slidesToScroll: 1,
 });
 });
+$('.nav-bar').on('click', function () {
+    $(this).toggleClass('active-bar');
+    $('.collapse').toggleClass('menu-open');
+});
+
+$('.nav a').on('click', function () {
+    $('.menu').removeClass('active-bar');
+    $('.collapse').removeClass('menu-open');
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
