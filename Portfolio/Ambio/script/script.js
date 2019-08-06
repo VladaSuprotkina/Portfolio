77ebc34beb9585d128955b231d61dfd84ccb7c13
Vladislava Suprotkina <vladislava.stk@gmail.com>
2019-08-06 18:52:04 +0300

    new

--- Portfolio/Ambio/script/script.js
+++ Portfolio/Ambio/script/script.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
         dots: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-});
-});
-$('.menu-bar').on('click', function () {
-    $('.nav-bar').toggleClass('active-bar');
-    $('.collapse').toggleClass('menu-open');
-});
-
-$('.nav-bar_collapse > a').on('click', function () {
-    $('.menu').removeClass('active-bar');
-    $('.collapse').removeClass('menu-open');
-});
-$('.nav-item').on('click', function () {
-    $(this).toggleClass('active');
-
-});
-$('.nav-item a').on('click', function () {
-    $('.nav-item').removeClass('active');
-});
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("nav");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+    $('.menu-bar').on('click', function () {
+        $('.nav-bar').toggleClass('active-bar');
+        $('.collapse').toggleClass('menu-open');
+    });
+
+    $('.nav-bar_collapse > a').on('click', function () {
+        $('.menu').removeClass('active-bar');
+        $('.collapse').removeClass('menu-open');
+    });
+    $('.nav-item').on('click', function () {
+        $(this).toggleClass('active');
+
+    });
+    $('.nav-item a').on('click', function () {
+        $('.nav-item').removeClass('active');
+    });
+
+        $(document).scroll(function () {
+            var $nav = $(".header_nav-bar");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
 });
