51787354fbfa872b37ca2298bc0577f636e0c79f
Vladislava Suprotkina <vladislava.stk@gmail.com>
2019-08-16 18:06:18 +0300

    new

--- Portfolio/health_happy/js/script.js
+++ Portfolio/health_happy/js/script.js
@@ -10,7 +10,28 @@ $(document).ready(function(){
         dots: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        // autoplay: true,
-        // autoplaySpeed: 5000
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1424,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.nav-bar_toggler').on('click', function () {
+        $('.open').toggleClass('open-menu');
     });
 });
